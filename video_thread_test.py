02e87933633f56e1b2329e8649f490afa9dcd1ce
theliamdavidson <misterliamd@gmail.com>
2022-12-07 14:01:20 -0800

    tests are functional, onto camera implementation

--- video_thread_test.py
+++ video_thread_test.py
@@ -3,35 +3,51 @@ import queue
 import concurrent.futures
 import random
 import logging
-import time
 import vessel_math as vm
 
+artery_num = 0
+message_list = []
+len_list = []
 
 def cam_cap(queue, event):
     """Pretend we're getting a number from the network."""
     while not event.is_set():
         message = random.randint(10, 99)
-        logging.info("captured: %s", message)
         queue.put(message, "Camera")
 
 def alg_store(queue, event):
     """Pretend we're saving a number in the database."""
     while not event.is_set() or not queue.empty():
         message = queue.get()
-        logging.info("stored: %s",message)
-        
+        if len(message_list) < 5:
+            message_list.append(message)
+        else:
+            if len(len_list) == 50:
+                event.set()
+            logging.info("run through the ligorithm: %s",message_list)
+            algThread = threading.Thread(target=patient_instance.converter, args=(message_list,))
+            algThread.start()
+            algThread.join()
+            message_list.clear()
+            len_list.append("i")
+            message_list.append(len(len_list))
+            
+            
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
     patient_instance = vm.Vessel_math("DavidsonLiam")
+    message_list.append(len(len_list))
     pipeline = queue.Queue(maxsize=10)
     event = threading.Event()
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         executor.submit(cam_cap, pipeline, event)
         executor.submit(alg_store, pipeline, event)
 
+    logging.info("done")
 
-        time.sleep(.2)
-        event.set()
+    vessel_group_var = patient_instance.macro_vessel_calculations()
+
+    print(vessel_group_var)
